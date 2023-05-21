b85f99c9e4ce8bde4326b3cecaa091f9eff4603f
Maximilian Schwenger <schwengermaximilian@gmail.com>
2023-05-21 16:01:09 +0200

    Fix scene time trigger.

--- src/scenes/manager.rs
+++ src/scenes/manager.rs
@@ -68,25 +68,18 @@ impl<'a> SceneEvaluator<'a> {
     match trigger {
       Trigger::And(a, b) => self.evaluate_trigger(a.as_ref()) && self.evaluate_trigger(b.as_ref()),
       Trigger::DeviceState(dst) => self.evaluate_update_trigger(dst),
-      Trigger::Time(TimeTrigger { from, duration }) => self.evaluate_time_trigger(*from, *duration),
+      Trigger::Time(TimeTrigger { from, duration }) => {
+        Self::evaluate_time_trigger(*from, *duration, Local::now().time())
+      }
     }
   }
 
-  fn evaluate_time_trigger(&self, from: NaiveTime, duration: Duration) -> bool {
-    let now = Local::now().time();
-    let res = if from < from + duration {
-      println!("{from} < {now} && {now} < {from} + {duration}.");
-      println!("{} && {}.", from < now, now < from + duration);
-      println!("{}.", from < now && now < from + duration);
+  fn evaluate_time_trigger(from: NaiveTime, duration: Duration, now: NaiveTime) -> bool {
+    if from < from + duration {
       from < now && now < from + duration
     } else {
-      println!("{from} < {now} && {now} > {from} + {duration}.");
-      println!("{} && {}.", from < now, now > from + duration);
-      println!("{}.", from < now && now > from + duration);
-      from < now && now > from + duration
-    };
-    println!("Evaluating timed trigger with result {res}.");
-    res
+      now > from || now < from + duration
+    }
   }
 
   fn evaluate_update_trigger(&self, dst: &DeviceStateTrigger) -> bool {
@@ -104,7 +97,7 @@ impl<'a> SceneEvaluator<'a> {
     match op {
       Comparison::BoolComparison { pivot } => field.as_bool().map(|c| c == *pivot).unwrap_or(false),
       Comparison::Equality { value } => {
-        println!("Comparing {value} against {}.", field.to_string());
+        println!("Comparing {value} against {}.", field);
         &field.to_string() == value
       }
     }
@@ -116,5 +109,37 @@ impl<'a> SceneEvaluator<'a> {
     payload.topic = Some(target.clone());
     assert_ne!(command, LightCommand::ChangeState);
     vec![Request::LightCommand(command, payload)]
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use chrono::{Duration, NaiveTime};
+
+  use super::SceneEvaluator;
+
+  #[test]
+  fn test_time_trigger_eval() {
+    let cases = vec![
+      (8, 4, 10, true),   // morning: basic
+      (8, 1, 10, false),  // morning: too late
+      (8, 1, 7, false),   // morning: too early
+      (16, 4, 18, true),  // afternoon: basic
+      (16, 4, 22, false), // afternoon: too late
+      (16, 4, 13, false), // afternoon: too early
+      (10, 4, 13, true),  // morning to afternoon: basic
+      (10, 4, 15, false), // morning to afternoon: too late
+      (10, 4, 9, false),  // morning to afternoon: too early
+      (22, 4, 1, true),   // afternoon to morning: basic
+      (22, 4, 3, false),  // afternoon to morning: too late
+      (22, 4, 21, false), // afternoon to morning: too early
+    ];
+    for case in cases {
+      let (from, duration, now, res) = case;
+      let from = NaiveTime::from_hms_opt(from, 0, 0).unwrap();
+      let duration = Duration::hours(duration);
+      let now = NaiveTime::from_hms_opt(now, 0, 0).unwrap();
+      assert_eq!(SceneEvaluator::evaluate_time_trigger(from, duration, now), res);
+    }
   }
 }
